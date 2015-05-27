41b8a2a4ce38c00759c9998a1faa41766e03ac42
mujx <siderisk@auth.gr>
2015-05-27 23:55:33 +0300

    Add ROS independent tests for world_model callback

--- tests/unit/world_model.py
+++ tests/unit/world_model.py
@@ -7,7 +7,7 @@
 from threading import Thread
 import unittest
 
-from mock import Mock, patch
+from mock import MagicMock, patch
 
 import roslib
 roslib.load_manifest('pandora_fsm')
@@ -19,15 +19,19 @@ from pandora_fsm.mocks import WorldModel
 from pandora_fsm import Agent, Navigator
 
 
+def empty():
+    """ Dummy patch function. """
+    pass
+
+
 class WorldModelSub(unittest.TestCase):
     """ Tests for the agent callbacks. """
 
     def setUp(self):
-        rospy.init_node('unit_world_model')
-        self.agent = Agent(strategy='normal')
+        self.agent = Agent(testing=True)
         self.world_model = WorldModel()
 
-    @unittest.skip('not ready')
+    @unittest.skip('ROS dependent')
     def test_connection(self):
         """ The receive_world_model should be called. """
 
@@ -41,81 +45,111 @@ class WorldModelSub(unittest.TestCase):
         self.assertIsNot(self.agent.visited_victims, [])
         self.assertIsNotNone(self.agent.target)
 
-    @unittest.skip('not ready')
-    def test_receive_world_model_with_target(self):
+    def test_update_target(self):
         """ Tests that the target is updated. """
 
-        # To assert point_of_interest
-        self.agent.state = 'exploration'
+        self.agent.IDENTIFICATION_THRESHOLD = 0.8
+        self.agent.VERIFICATION_THRESHOLD = 0.8
 
-        # Create a victim and assign it to the target.
-        target = mock_msgs.create_victim_info()
-        self.agent.target_victim = target
-
-        self.assertEqual(self.agent.target_victim.id, target.id)
-
-        # Create a custom world_model msg with updated target.
-        target.probability = 0.8
-        visited = [mock_msgs.create_victim_info() for i in range(2)]
-        current_victims = [target]
-        model = mock_msgs.create_world_model(current_victims, visited)
-
-        self.agent.receive_world_model(model)
-        self.assertEqual(self.agent.target_victim.id, target.id)
-        self.assertEqual(self.agent.target_victim.probability, 0.8)
-        self.assertTrue(self.agent.point_of_interest.is_set())
-
-    @unittest.skip('not ready')
-    def test_receive_world_model_identification_threshold(self):
-        """ Tests that the promising_victim event is set. """
-
-        # To assert point_of_interest
-        self.agent.state = 'exploration'
-        self.agent.IDENTIFICATION_THRESHOLD = 0.5
-        target = mock_msgs.create_victim_info(probability=0.7)
-        self.agent.target_victim = target
-
-        visited = [mock_msgs.create_victim_info() for i in range(2)]
-        current_victims = [target]
-        model = mock_msgs.create_world_model(current_victims, visited)
-
-        self.agent.receive_world_model(model)
-        self.assertEqual(self.agent.target_victim.id, target.id)
-        self.assertTrue(self.agent.promising_victim.is_set())
-
+        target = mock_msgs.create_victim_info(id=1)
         target.probability = 0.2
+
+        self.agent.target = target
+        visited = [mock_msgs.create_victim_info() for i in range(2)]
         current_victims = [target]
         model = mock_msgs.create_world_model(current_victims, visited)
 
-        self.agent.receive_world_model(model)
-        self.assertEqual(self.agent.target_victim.id, target.id)
-        self.assertFalse(self.agent.promising_victim.is_set())
+        with patch.object(Agent, 'update_target_victim') as mock:
+            mock.side_effect = empty
+            self.agent.receive_world_model(model)
 
-    @unittest.skip('not ready')
-    def test_receive_world_model_verification_threshold(self):
-        """ Tests that the recognized_victim event is set. """
+            self.assertTrue(mock.called)
+            self.assertFalse(self.agent.probable_victim.is_set())
+            self.assertFalse(self.agent.victim_verified.is_set())
 
-        # To assert point_of_interest
-        self.agent.state = 'exploration'
-        self.agent.VERIFICATION_THRESHOLD = 0.5
-        target = mock_msgs.create_victim_info(probability=0.7)
-        self.agent.target_victim = target
+    def test_choose_new_victim(self):
+        """ Tests that the target is updated. """
+
+        target = mock_msgs.create_victim_info(id=1)
+        target.probability = 0.2
 
         visited = [mock_msgs.create_victim_info() for i in range(2)]
         current_victims = [target]
         model = mock_msgs.create_world_model(current_victims, visited)
 
-        self.agent.receive_world_model(model)
-        self.assertEqual(self.agent.target_victim.id, target.id)
-        self.assertTrue(self.agent.recognized_victim.is_set())
+        with patch.object(Agent, 'update_target_victim') as mock_update:
+            mock_update.side_effect = empty
 
+            self.agent.receive_world_model(model)
+
+            self.assertFalse(mock_update.called)
+            self.assertEqual(self.agent.target, target)
+            self.assertFalse(self.agent.probable_victim.is_set())
+            self.assertFalse(self.agent.victim_verified.is_set())
+
+    def test_poi_found(self):
+        """ Tests that the target is updated. """
+
+        self.agent.state = 'exploration'
+        target = mock_msgs.create_victim_info(id=1)
         target.probability = 0.2
+        self.agent.target = target
+
+        visited = [mock_msgs.create_victim_info() for i in range(2)]
         current_victims = [target]
         model = mock_msgs.create_world_model(current_victims, visited)
 
-        self.agent.receive_world_model(model)
-        self.assertEqual(self.agent.target_victim.id, target.id)
-        self.assertFalse(self.agent.recognized_victim.is_set())
+        with patch.object(Agent, 'update_target_victim') as mock_update:
+            mock_update.side_effect = empty
+
+            self.agent.receive_world_model(model)
+
+            self.assertTrue(mock_update.called)
+            self.assertEqual(self.agent.target, target)
+            self.assertFalse(self.agent.probable_victim.is_set())
+            self.assertFalse(self.agent.victim_verified.is_set())
+
+    def test_identification_threshold(self):
+        """ Tests that the probable_victim event is set. """
+
+        self.agent.IDENTIFICATION_THRESHOLD = 0.6
+        self.agent.VERIFICATION_THRESHOLD = 0.8
+
+        target = mock_msgs.create_victim_info(probability=0.7)
+        self.agent.target = target
+
+        visited = [mock_msgs.create_victim_info() for i in range(2)]
+        current_victims = [target]
+        model = mock_msgs.create_world_model(current_victims, visited)
+
+        with patch.object(Agent, 'update_target_victim') as mock:
+            mock.side_effect = empty
+            self.agent.receive_world_model(model)
+
+            self.assertTrue(mock.called)
+            self.assertTrue(self.agent.probable_victim.is_set())
+            self.assertFalse(self.agent.victim_verified.is_set())
+
+    def test_verification_threshold(self):
+        """ Tests that the victim_verified event is set. """
+
+        self.agent.IDENTIFICATION_THRESHOLD = 0.8
+        self.agent.VERIFICATION_THRESHOLD = 0.6
+
+        target = mock_msgs.create_victim_info(probability=0.7)
+        self.agent.target = target
+
+        visited = [mock_msgs.create_victim_info() for i in range(2)]
+        current_victims = [target]
+        model = mock_msgs.create_world_model(current_victims, visited)
+
+        with patch.object(Agent, 'update_target_victim') as mock:
+            mock.side_effect = empty
+            self.agent.receive_world_model(model)
+
+            self.assertTrue(mock.called)
+            self.assertFalse(self.agent.probable_victim.is_set())
+            self.assertTrue(self.agent.victim_verified.is_set())
 
 
 class UpdateTarget(unittest.TestCase):
@@ -123,17 +157,13 @@ class UpdateTarget(unittest.TestCase):
     def setUp(self):
         self.agent = Agent(testing=True)
 
-    def empty(self):
-        """ Dummy patch function. """
-        pass
-
     def test_with_no_update(self):
         """ The current victim cannot be updated. """
 
         target = mock_msgs.create_victim_info(id=1, probability=0.6)
         self.agent.target = target
         self.current_victims = []
-        approach_target = Mock(spec=self.agent.approach_target)
+        approach_target = MagicMock(spec=self.agent.approach_target)
 
         self.agent.update_target_victim()
 
@@ -153,8 +183,8 @@ class UpdateTarget(unittest.TestCase):
         another = mock_msgs.create_victim_info(id=2, probability=0.3)
         self.agent.target = target
         self.agent.current_victims = [another, target_updated]
-        approach_target = Mock(spec=self.agent.approach_target)
-        cancel_all_goals = Mock(spec=self.agent.navigator.cancel_all_goals)
+        approach_target = MagicMock(spec=self.agent.approach_target)
+        cancel_all_goals = MagicMock(spec=self.agent.navigator.cancel_all_goals)
 
         self.agent.update_target_victim()
 
@@ -175,8 +205,8 @@ class UpdateTarget(unittest.TestCase):
         another = mock_msgs.create_victim_info(id=2, probability=0.3)
         self.agent.target = target
         self.agent.current_victims = [another, target_updated]
-        approach_target = Mock(spec=self.agent.approach_target)
-        cancel_all_goals = Mock(spec=self.agent.navigator.cancel_all_goals)
+        approach_target = MagicMock(spec=self.agent.approach_target)
+        cancel_all_goals = MagicMock(spec=self.agent.navigator.cancel_all_goals)
 
         self.agent.update_target_victim()
 
@@ -206,8 +236,8 @@ class UpdateTarget(unittest.TestCase):
             with patch.object(Agent, 'approach_target') as mock_agent:
 
                 # Change the implementation of the functions.
-                mock_control.side_effect = self.empty
-                mock_agent.side_effect = self.empty
+                mock_control.side_effect = empty
+                mock_agent.side_effect = empty
 
                 self.agent.update_target_victim()
 
