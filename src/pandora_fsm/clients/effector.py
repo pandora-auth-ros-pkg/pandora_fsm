3bd66b8cbeb891f851d7ea34a88bdd003c418c5c
mujx <siderisk@auth.gr>
2015-07-22 08:53:22 +0800

    Change the center frame of the linear motor

--- src/pandora_fsm/clients/effector.py
+++ src/pandora_fsm/clients/effector.py
@@ -37,7 +37,7 @@ class Effector(object):
         self.client.cancel_all_goals()
         sleep(3)
 
-    def point_to(self, target, center='/picam_frame'):
+    def point_to(self, target, center='/front_picam_frame'):
         """ Points end effector to a target.
 
         :param target: The victim frame ID.
@@ -53,7 +53,7 @@ class Effector(object):
         log.info('Sending TRACK goal.')
         self.client.send_goal(goal)
 
-    def slowly_point_to(self, target, center='/picam_frame'):
+    def slowly_point_to(self, target, center='/front_picam_frame'):
         """
         Points end effector to a target, slow enough so the image
         is staying still.
